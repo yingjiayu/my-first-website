a9d615731ee557a102af13c7fee80dfb46af1738
Your Name <yingjia.yu@student.unimelb.edu.au>
2026-05-10 00:18:27 +1000

    breadcrumbs part

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//drop down part in product list's filters
 function dropdownSection(whichSection, triggerPart) {
   const box = document.querySelector(whichSection);
   const trigger = document.querySelector(triggerPart);
@@ -12,9 +13,35 @@ function dropdownSection(whichSection, triggerPart) {
     }
   });
 }
-
-// 初始化所有 dropdown
-
 dropdownSection('.filter-sortby', '.filter-sortby-title');
-
 dropdownSection('.filter-collection', '.filter-collection-title');
+
+
+
+function getQueryParam(name) {
+  const params = new URLSearchParams(window.location.search);
+  return params.get(name);
+}
+function renderBreadcrumb(collectionName = null) {
+  const breadcrumb = document.querySelector('.breadcrumb');
+  const from = getQueryParam('from');
+  let html = `<a href="index.html">Home</a> ▸ `;
+  if (from === 'product') {
+    html += `<a href="productlist.html?from=home">All Products</a>`;
+  } else {
+    html += `<span>All Products</span>`;
+  }
+  if (collectionName) {
+    html += ` ▸ <span>${collectionName}</span>`;
+  }
+  breadcrumb.innerHTML = html;
+}
+
+renderBreadcrumb();
+
+document.querySelectorAll('.filter-collection-li').forEach(item => {
+  item.addEventListener('click', () => {
+    const name = item.textContent;
+    renderBreadcrumb(name);
+  });
+});
