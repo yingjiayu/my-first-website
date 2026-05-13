4c81fb70f4aef8bf33d5ed5f1c86637eb5b9fc4b
Your Name <yingjia.yu@student.unimelb.edu.au>
2026-05-13 23:55:48 +1000

    product page with using of clamp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//drop down part in product list's filters
+//drop down section in product list's filters
 function dropdownSection(whichSection, triggerPart) {
   const box = document.querySelector(whichSection);
   const trigger = document.querySelector(triggerPart);
@@ -13,35 +13,9 @@ function dropdownSection(whichSection, triggerPart) {
     }
   });
 }
+dropdownSection('.shop-dropdown', '.nav-item-left-shop');
 dropdownSection('.filter-sortby', '.filter-sortby-title');
 dropdownSection('.filter-collection', '.filter-collection-title');
-
-
-
-function getQueryParam(name) {
-  const params = new URLSearchParams(window.location.search);
-  return params.get(name);
-}
-function renderBreadcrumb(collectionName = null) {
-  const breadcrumb = document.querySelector('.breadcrumb');
-  const from = getQueryParam('from');
-  let html = `<a href="index.html">Home</a> ▸ `;
-  if (from === 'product') {
-    html += `<a href="productlist.html?from=home">All Products</a>`;
-  } else {
-    html += `<span>All Products</span>`;
-  }
-  if (collectionName) {
-    html += ` ▸ <span>${collectionName}</span>`;
-  }
-  breadcrumb.innerHTML = html;
-}
-
-renderBreadcrumb();
-
-document.querySelectorAll('.filter-collection-li').forEach(item => {
-  item.addEventListener('click', () => {
-    const name = item.textContent;
-    renderBreadcrumb(name);
-  });
-});
+dropdownSection('.product-description-one', '.product-description-trigger');
+dropdownSection('.product-description-two', '.product-description-trigger-two');
+dropdownSection('.product-description-three', '.product-description-trigger-three');
